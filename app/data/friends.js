d9e7ce67963e998ed80967aad6e627f42445ab0d
Don Guckenberger <dguck23@gmail.com>
2017-08-15 12:49:09 -0600

    works with launching the server (after changing my express(express()) issue on the require for htmlRoute

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,10 @@
+
 var peopleData = [
 {
-	name: Don,
+	name: 'Don',
 	photo: 'http://www.fabioifc.com/Bookcover_plus/FABIO_head_shot_tank.jpg',
 	answers: [3,3,3,3,3,3,3,3,3,3]
 }
 ];
 
-module.exports() = peopleData;
+module.exports = peopleData;
--- app/routing/htmlRoute.js
+++ app/routing/htmlRoute.js
@@ -3,8 +3,7 @@
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
 var path = require("path");
-var express = require('express');
-var app = express();
+
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -15,17 +14,21 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  app.get("/", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "../public/home.html"));
   });
 
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "../public/survey.html"));
+  });
   // If no matching route is found default to home
-  app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  // app.use(function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/home.html"));
+  // });
+
+  //POST survey results 
+ app.post("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 };
 
-//POST survey results 
- app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
-  });
